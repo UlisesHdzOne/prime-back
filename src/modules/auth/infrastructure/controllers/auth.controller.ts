180119eb71436c2c6a95009fd8611248c0216d76
UlisesHdzOne <ulisesdejesusk9999@gmail.com>
2025-07-22 16:40:35 -0600

    feat(auth): mejorar docs Swagger en endpoint register, agregar comentarios y .env.example

--- src/modules/auth/infrastructure/controllers/auth.controller.ts
+++ src/modules/auth/infrastructure/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { RegisterUseCase } from '../../application/use-cases/register.use-case';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RegisterDto } from '../../application/dto/register.dto';
 
 @ApiTags('auth')
@@ -10,20 +10,35 @@ export class AuthController {
     private registerUseCase: RegisterUseCase
   ) {}
 
+  // ==== REGISTER ====
   @Post('register')
   @ApiOperation({
     summary: 'Registrar un nuevo usuario',
-    description: 'Ruta para crear un nuevo usurio en el sistema',
+    description: 'Crea un usuario si el correo no ha sido registrado',
   })
+  @ApiBody({ type: RegisterDto })
   @ApiResponse({
     status: 201,
     description: 'Usuario registrado exitosamente',
     schema: {
-      example: { name: 'Juan', email: 'juan@mail.com' },
+      example: { id: 1, name: 'Juan', email: 'juan@mail.com' },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Correo electrónico ya registrado',
+    schema: {
+      example: {
+        statusCode: 400,
+        message: 'Correo electrónico ya registrado',
+        error: 'Bad Request',
+      },
     },
   })
   async register(@Body() dto: RegisterDto) {
     const user = await this.registerUseCase.execute(dto);
     return { id: user.id, name: user.name, email: user.email };
   }
+  // ==== REGISTER ====
+
 }
